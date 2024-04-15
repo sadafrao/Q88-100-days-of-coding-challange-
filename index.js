d154eb3cd16c4cc7dc2f84b1e5d37ae2e89a5fa3
SADAF SOHAIL <159582784+sadafrao@users.noreply.github.com>
2024-04-15 15:28:11 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+// Q88
+// this function rounding a nearst integer with decimal number
+function nearstinteger(num) {
+    return Math.round(num); // round the number
+}
+console.log(nearstinteger(3.4));
---
+++ index.ts
@@ -0,0 +1,6 @@
+// Q88
+// this function rounding a nearst integer with decimal number
+function nearstinteger(num : number): number {
+return Math.round(num);// round the number
+}
+console.log(nearstinteger(3.4));// output is 3
